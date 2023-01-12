2f87496854415c8cb7aaabbf44932aeae02fe701
miftah24 <miftahuddin241199@gmail.com>
2023-01-12 21:01:04 +0700

    commit

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bank_mf/shared/theme.dart';
 import 'package:flutter/material.dart';
 
@@ -8,49 +10,65 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: lightBackgroundColor,
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: blueColor,
-        unselectedItemColor: blackColor,
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        selectedLabelStyle: blueTextStyle.copyWith(
-          fontSize: 10,
-          fontWeight: medium,
-        ),
-        unselectedLabelStyle: blackTextStyle.copyWith(
-          fontSize: 10,
-          fontWeight: medium,
-        ),
-        items: [
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic_overview.png',
-              width: 20,
-              color: blueColor,
-              ), 
-              label: 'Overview',
-            ),
+      bottomNavigationBar: BottomAppBar(
+        color: whiteColor,
+        shape: const CircularNotchedRectangle(),
+        clipBehavior: Clip.antiAlias,
+        notchMargin: 6,
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          elevation: 0,
+          selectedItemColor: blueColor,
+          unselectedItemColor: blackColor,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          selectedLabelStyle: blueTextStyle.copyWith(
+            fontSize: 10,
+            fontWeight: medium,
+          ),
+          unselectedLabelStyle: blackTextStyle.copyWith(
+            fontSize: 10,
+            fontWeight: medium,
+          ),
+          items: [
             BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic_history.png',
-              width: 20,
-              ), 
-              label: 'History',
-            ),BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic_statistic.png',
-              width: 20,
-              ), 
-              label: 'Statistic',
-            ),BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic_reward.png',
-              width: 20,
-              ), 
-              label: 'Reward',
-            ),
-        ],
-        ),
+              icon: Image.asset(
+                'assets/ic_overview.png',
+                width: 20,
+                color: blueColor,
+                ), 
+                label: 'Overview',
+              ),
+              BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_history.png',
+                width: 20,
+                ), 
+                label: 'History',
+              ),BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_statistic.png',
+                width: 20,
+                ), 
+                label: 'Statistic',
+              ),BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_reward.png',
+                width: 20,
+                ), 
+                label: 'Reward',
+              ),
+          ],
+          ),
+      ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Image.asset(
+          'assets/ic_plus_circle.png',
+          width: 24,
+          ),
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
